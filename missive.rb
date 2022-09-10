a739e23a225cf36b92711f1230981254b83aea03
Rory <n/a>
2022-09-10 09:42:32 +0100

    feat: add more info to missive, prepare for print

--- missive.rb
+++ missive.rb
@@ -1,13 +1,36 @@
 class Missive
 
-  attr_reader :name, :message
+  attr_reader :location, :gathering, :creation_time, :name, :message
 
   def initialize(name, message)
+    @location = "Laniakea Supercluster, Virgo Cluster, Local Group, Milky Way, Earth, Cymru"
+    @gathering = "Microburn 2022 (CE)"
+    @creation_time = Time.now
     @name = name
     @message = message
   end
 
   def prepare_for_printing
-    [name, message]
+    [prep_time, prep_location, prep_name, prep_gathering, prep_message]
+  end
+
+  def prep_location
+    "Scribing location: " + location
+  end
+
+  def prep_gathering
+    "Gathering: " + gathering
+  end
+
+  def prep_time
+    "Scribing time: " + creation_time.to_s
+  end
+
+  def prep_name
+    "Scribe: " + name
+  end
+
+  def prep_message
+    "Missive contents: " + message
   end
 end
