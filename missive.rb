20f5a6e844f87a568fa063a6aefade3563262623
Rory <n/a>
2022-09-12 15:44:31 +0100

    feat: user can input a new missive, missive is saved in memory

--- missive.rb
+++ missive.rb
@@ -2,12 +2,14 @@ class Missive
 
   attr_reader :location, :gathering, :creation_time, :name, :message
 
-  def initialize(name, message)
+  def initialize(name, temporal_identifier, message)
+    @name = name
+    @temporal_identifier = temporal_identifier
+    @message = message
     @location = "Laniakea Supercluster, Virgo Cluster, Local Group, Milky Way, Earth, Cymru"
     @gathering = "Microburn 2022 (CE)"
     @creation_time = Time.now
-    @name = name
-    @message = message
+
   end
 
   def prepare_for_printing
@@ -15,22 +17,22 @@ class Missive
   end
 
   def prep_location
-    "Scribing location: " + location
+    ["Scribing location: " + location, 0.02]
   end
 
   def prep_gathering
-    "Gathering: " + gathering
+    ["Gathering: " + gathering, 0.06]
   end
 
   def prep_time
-    "Scribing time: " + creation_time.to_s
+    ["Scribing time: " + creation_time.to_s, 0.04]
   end
 
   def prep_name
-    "Scribe: " + name
+    ["Scribe: " + name, 0.1]
   end
 
   def prep_message
-    "Missive contents: " + message
+    ["Missive contents:\n" + message, 0.02]
   end
 end
--- operator.rb
+++ operator.rb
@@ -1,15 +1,53 @@
 require "./missive.rb"
+require 'io/console'
 
 class Operator
 
   def run
-    puts "What is your name?"
-    name = gets.chomp
+    basic_input
+
     if name == "Ω"
       puts "Resetting"
     else
       puts name
     end
+  end
+
+  def basic_input
+    puts "What is your name?"
+    puts
+    name = gets.chomp
+    puts
+    puts "Enter a temporal identifier for yourself in the form of 4 digits e.g. '0806'.\nThis will allow your missives to be linked across time."
+    puts
+    temporal_identifier = gets.chomp
+    puts
+    puts "Enter the missive you would like to send to the future. Hit enter when you have finished. Warning: once you have hit enter you cannot edit your missive."
+    puts
+    missive_message = gets.chomp
+    puts
+    puts "Are you ready to submit your missive? [y] to confirm, [c] to delete and start again."
+    submission_command = gets.chomp
+    submission_handler(submission_command)
+  end
+
+  def submission_handler(command)
+    case command
+    when 'y'
+      create_new_missive(name, temporal_identifier, missive_message)
+    when 'c'
+      puts
+      basic_input
+    else
+      puts "Command not recognised, input command again:"
+      submission_command = gets.chomp
+      submission_handler(submission_command)
+    end
+    puts
+  end
+
+  def create_new_missive(name, temporal_identifier, missive_message)
+    Missive.new(name, temporal_identifier, missive_message)
   end
 
   def input_name; end
@@ -23,18 +61,24 @@ class Operator
   def epsilon_present?; end
 
   def display_missive
-    parts = Missive.new("Rory", "This is a test missive. Print me slowly.").prepare_for_printing
-    parts.each {|part| print_text(part)}
+    parts = Missive.new("Rory", "This is a test missive. Print me slowly. Or quickly. Quickly probably better to be honest, you have already made people wait in anticipation with the slow roll above.").prepare_for_printing
+    parts.each {|part| print_text(part[0], part[1])}
   end
 
-  def print_text(text)
+  def print_text(text, print_rate)
     split_chars = text.split("")
     recombined_chars = []
 
-    split_chars.each do |char|
-      recombined_chars << char
-      sleep(0.1)
-      print recombined_chars.join + "\r"
+    terminal_columns_count = IO.console.winsize[1]
+
+    if text.length >= terminal_columns_count
+      print text
+    else
+      split_chars.each do |char|
+        recombined_chars << char
+        sleep(print_rate)
+        print recombined_chars.join + "\r"
+      end
     end
 
     puts
@@ -42,4 +86,4 @@ class Operator
   end
 end
 
-Operator.new.display_missive
+Operator.new.run
