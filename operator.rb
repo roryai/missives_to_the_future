b85c5c8c4283ae4418e958c274e11ffb20841f81
Rory <n/a>
2022-09-12 16:20:01 +0100

    feat: MVP working. adds memory storage, read and write functions

--- operator.rb
+++ operator.rb
@@ -1,53 +1,87 @@
 require "./missive.rb"
+require 'pry'
 require 'io/console'
 
 class Operator
 
-  def run
-    basic_input
+  def initialize
+    @missive_storage = []
+  end
 
-    if name == "Ω"
-      puts "Resetting"
+  def run
+    read_or_write
+  end
+
+  def read_or_write
+    puts
+    puts "Do you want to read a missive, or write one?\n[r] to read, [w] to write:"
+    puts
+    command = gets.chomp
+    puts
+    read_or_write_handler(command)
+  end
+
+  def read_or_write_handler(command)
+    case command
+    when "r"
+      display_missive
+    when "w"
+      basic_input
     else
-      puts name
+      "Command not recognised.\n[[r] to read, [w] to write\ Input command again:"
+      command = gets.chomp
+      read_or_write_handler(command)
     end
+  end
+
+  def display_missive
+    @missive_storage << Missive.new("Rory", "1337", "This is a test missive. Print me slowly. Or quickly. Quickly probably better to be honest, you have already made people wait in anticipation with the slow roll above.")
+
+    selected_missive = @missive_storage.shuffle.first
+
+    parts = selected_missive.prepare_for_printing
+    parts.each {|part| print_text(part[0], part[1])}
+
+    read_or_write
   end
 
   def basic_input
     puts "What is your name?"
     puts
-    name = gets.chomp
+    @name = gets.chomp
     puts
     puts "Enter a temporal identifier for yourself in the form of 4 digits e.g. '0806'.\nThis will allow your missives to be linked across time."
     puts
-    temporal_identifier = gets.chomp
+    @temporal_identifier = gets.chomp
     puts
     puts "Enter the missive you would like to send to the future. Hit enter when you have finished. Warning: once you have hit enter you cannot edit your missive."
     puts
-    missive_message = gets.chomp
+    @missive_message = gets.chomp
     puts
-    puts "Are you ready to submit your missive? [y] to confirm, [c] to delete and start again."
+    puts "Are you ready to submit your missive?\n[s] to submit, [d] to delete and start again."
     submission_command = gets.chomp
     submission_handler(submission_command)
   end
 
   def submission_handler(command)
     case command
-    when 'y'
-      create_new_missive(name, temporal_identifier, missive_message)
-    when 'c'
+    when 's'
+      create_new_missive(@name, @temporal_identifier, @missive_message)
+    when 'd'
       puts
       basic_input
     else
-      puts "Command not recognised, input command again:"
+      puts "Command not recognised.\n[s] to submit, [d] to delete and start again\ Input command again:"
       submission_command = gets.chomp
       submission_handler(submission_command)
     end
     puts
+
+    read_or_write
   end
 
   def create_new_missive(name, temporal_identifier, missive_message)
-    Missive.new(name, temporal_identifier, missive_message)
+    @missive_storage << Missive.new(name, temporal_identifier, missive_message)
   end
 
   def input_name; end
@@ -59,11 +93,6 @@ class Operator
   def reset_process; end
 
   def epsilon_present?; end
-
-  def display_missive
-    parts = Missive.new("Rory", "This is a test missive. Print me slowly. Or quickly. Quickly probably better to be honest, you have already made people wait in anticipation with the slow roll above.").prepare_for_printing
-    parts.each {|part| print_text(part[0], part[1])}
-  end
 
   def print_text(text, print_rate)
     split_chars = text.split("")
